54f53f3bd41d888d84e2719c18c98cb4939d4896
kizzual <67533735+kizzual@users.noreply.github.com>
2022-08-19 00:15:25 +0700

    update text

--- Assets/Scripts/Raid/Raid_UI.cs
+++ Assets/Scripts/Raid/Raid_UI.cs
@@ -64,7 +64,7 @@ public class Raid_UI : MonoBehaviour
         m_currentHero.currentRaidSlot = SlotNumber;
         Rank.text = "D " + hero.cube.edgesNumber.ToString();
         lvl.text = hero.Level.ToString();
-        combo.text = hero.GetGoldProfit().ToString();
+        combo.text = ConvertText.FormatNumb(hero.GetGoldProfit());
         goldToGrade.text = ConvertText.FormatNumb(hero.GoldToGrade);
         heroIcon.sprite = hero.Icon;
         foreach (var item in elementArrow)
